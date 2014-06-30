5848e3d7e69fb51ee4067c65590dc4f66d00b3f0
LeTest <author@example.com>
2014-06-30 01:00:00 +0100

    2014-06-30T1:00

--- code.sh
+++ code.sh
@@ -1,3 +1,3 @@
-Ns7cMlS5dQQXVkzyKQPCPsGlDbkdLqzl
-ztfQaPtmcqqnVrewZFheV5nHeYzq3N16
-saNbcGErHHKTZSpmcXB3DmkV2I0HQ8Zi
+DcqAtM6BjOMxns6mx5ZVyGphoJBY7CHi
+GYep8IhWheRwPkLogdsbesNfJa6FjCoj
+gceE6KQbpDSSN2wPZ8FQckQtkqnTLr8K
